a48bcd09130da92dbe54ca10af8098cb0d219acd
Takashi Kawanaka <takashi.soccer.a@gmail.com>
2016-10-20 14:40:06 +0900

    Edit main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include "average.h"
+#include "sum.h"
 int main ()
 {
 	int payments[5];
@@ -7,4 +8,6 @@ int main ()
 	for(i=0;i<5;i++) {
 		scanf("%d", &payments[i]);
 	}
+	printf("平均値：%d\n",average(payments));
+	printf("合計値：%d\n",sum(payments));
 }
